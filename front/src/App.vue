7dad58187d8105e8090c98ac383d9007f2f6f208
eunjin <asd0755@naver.com>
2024-01-16 14:40:48 +0900

    [#10] feat : add MainNav

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,7 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// import VLayout from '@/components/common/VLayout.vue'
+import MainNav from '@/components/common/MainNav.vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+</script>
 
 <template>
   <div>
+    <MainNav v-if="route.meta.layout == 'main'" />
     <RouterView />
   </div>
 </template>
