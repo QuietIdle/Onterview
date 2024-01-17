bcc4e792be01eb91ec61fa79c58076b7aa97d907
zqwz <zqwz@naver.com>
2024-01-17 11:30:44 +0900

    add: 테스트 파일 추가

---
+++ onterview-storage/src/test/java/com/quiet/onterviewstorage/OnterviewStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiet.onterviewstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnterviewStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
