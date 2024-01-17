f016670089406ef16065de9880ff8ebaee42197c
김승배 <ksshc@snu.ac.kr>
2024-01-17 17:17:52 +0900

    [#19 #20] feat : Self Speech Record / Tab 컴포넌트 개발

--- front/src/components/selfSpeech/SelfSpeechTab.vue
+++ front/src/components/selfSpeech/SelfSpeechTab.vue
@@ -1,7 +1,31 @@
-<script setup lang="ts"></script>
+<script setup>
+import { ref } from 'vue';
+
+const items = ref([
+        {
+          title: '나의 답변',
+          value: 1,
+        },
+        {
+          title: '영상 보관함',
+          value: 2,
+        },
+        {
+          title: '자가 진단',
+          value: 3,
+        },
+])
+</script>
 
 <template>
-  <div></div>
+    <v-card class="mr-auto" max-width="300">
+      <v-list :items="items" class="pa-0"></v-list>
+    </v-card>
+    <div class="content-container w-75 h-auto bg-yellow">
+
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
