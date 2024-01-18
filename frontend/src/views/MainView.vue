503eea4960191428f1ad8d702b046ce0db645814
eunjin <asd0755@naver.com>
2024-01-18 16:51:05 +0900

    [#333] feat : Main View 캐러셀 개발

--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -2,14 +2,30 @@
 import introduceImage1 from '@/assets/main/introduceImage1.png'
 import introduceImage2 from '@/assets/main/introduceImage2.png'
 import introduceImage3 from '@/assets/main/introduceImage3.png'
+
+const images = [introduceImage1, introduceImage2, introduceImage3]
+const titles = ['Onterview', '셀프 스피치', '모의 면접']
+const contents = [
+  '우리의 서비스는 탁월하다',
+  '혼자서도 면접 경험을 쌓자',
+  '여럿이서 면접 연습 경험도 쌓아보자'
+]
 </script>
 
 <template>
   <div>
-    <v-carousel>
-      <v-carousel-item :src="introduceImage1" cover></v-carousel-item>
-      <v-carousel-item :src="introduceImage2" cover></v-carousel-item>
-      <v-carousel-item :src="introduceImage3" cover></v-carousel-item>
+    <v-carousel cycle height="94vh" hide-delimiter-background show-arrows="hover">
+      <v-carousel-item v-for="(image, i) in images" :key="i" :src="image" cover>
+        <v-sheet
+          height="100%"
+          style="background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5))"
+        >
+          <div class="d-flex fill-height flex-column justify-center" style="margin-left: 10%">
+            <div class="text-h3 text-white">{{ titles[i] }}</div>
+            <div class="text-p text-white mt-3">{{ contents[i] }}</div>
+          </div>
+        </v-sheet>
+      </v-carousel-item>
     </v-carousel>
   </div>
 </template>
