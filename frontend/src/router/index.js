61102a36f1f6703f3ed310b31f380d682de5f5c3
eunjin <asd0755@naver.com>
2024-01-18 10:51:21 +0900

    [#366] init : update front files

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,23 +1,55 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'main',
+      component: () => import('@/views/MainView.vue'),
+      meta: {layout: 'main'},
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/UserLoginView.vue'),
+      meta: {layout: 'main'},
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('@/views/UserRegisterView.vue'),
+      meta: {layout: 'main'},
+    },
+    {
+      path: '/selfspeech',
+      name: 'selfspeech-main',
+      component: () => import('@/views/SelfSpeechMainView.vue'),
+      meta: {layout: 'main'},
+    },
+    {
+      path: '/selfspeech/room',
+      name: 'selfspeech-room',
+      component: () => import('@/views/SelfSpeechRoomView.vue'),
+      meta: {layout: ''},
+    },
+    {
+      path: '/storage/question',
+      name: 'storage-question',
+      component: () => import('@/views/StorageQuestionView.vue'),
+      meta: {layout: 'main'},
+    },
+    {
+      path: '/storage/video',
+      name: 'storage-video',
+      component: () => import('@/views/StorageVideoView.vue'),
+      meta: {layout: 'main'},
+    },
   ]
 })
 
 export default router
+
+
+
+
