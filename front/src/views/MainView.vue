ad80b8691127775ace4edc27e41464a901c5ec8f
eunjin <asd0755@naver.com>
2024-01-18 10:15:17 +0900

    [#332] feat : add Main View carousel

--- front/src/views/MainView.vue
+++ front/src/views/MainView.vue
@@ -1,8 +1,18 @@
-<script setup lang="ts"></script>
+<script setup>
+import introduceImage1 from '@/assets/main/introduceImage1.png'
+import introduceImage2 from '@/assets/main/introduceImage2.png'
+import introduceImage3 from '@/assets/main/introduceImage3.png'
+</script>
 
 <template>
   <div>
-    <h1>Main View 프리텐다드</h1>
+    <v-carousel hide-delimiters>
+      <v-carousel-item :src="introduceImage1" cover></v-carousel-item>
+
+      <v-carousel-item :src="introduceImage2" cover></v-carousel-item>
+
+      <v-carousel-item :src="introduceImage3" cover></v-carousel-item>
+    </v-carousel>
   </div>
 </template>
 
