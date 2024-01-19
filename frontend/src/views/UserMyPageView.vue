fa53576c887a543c3318892ac809658213906ce3
johyonghoon <johyonghoon01@gmail.com>
2024-01-19 11:34:48 +0900

    [#349] feat : UserMyPageView 섹션 분리

--- frontend/src/views/UserMyPageView.vue
+++ frontend/src/views/UserMyPageView.vue
@@ -1,7 +1,12 @@
 <script setup></script>
 
 <template>
-  <div></div>
+  <div class="offset-2 v-col-8">
+    <div>MyPage Nav</div>
+    <div>Update Profile</div>
+    <div>Change Password</div>
+    <div>Remove Member</div>
+  </div>
 </template>
 
 <style scoped></style>
