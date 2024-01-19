5aa011ca28031d41605ea32ceb92fe454e50f4f7
zqwz <zqwz@naver.com>
2024-01-19 15:23:07 +0900

    [#S10P12A504-44] feat: 파일 Response Dto 추가

--- onterview-storage/src/main/java/com/quiet/onterviewstorage/file/FileDto.java
+++ onterview-storage/src/main/java/com/quiet/onterviewstorage/file/FileDto.java
@@ -7,6 +7,14 @@ public class FileDto {
 
     @Getter
     @RequiredArgsConstructor
+    public static class FileResponse {
+
+        private final String filePath;
+        private final String fileName;
+    }
+
+    @Getter
+    @RequiredArgsConstructor
     public static class VideoResponse {
 
         private final String videoUrl;
