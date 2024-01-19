b87c1c3629288f5cd0658769550912c697dfcee9
chlwodyd9829 <chlwodyd9829@naver.com>
2024-01-19 09:45:08 +0900

    [#S10P12A504-45-api] add: Builder 추가

--- backend/src/main/java/com/quiet/onterview/video/entity/Video.java
+++ backend/src/main/java/com/quiet/onterview/video/entity/Video.java
@@ -14,6 +14,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -21,6 +22,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Getter
 @Entity
+@Builder
 @Table(name = "VIDEO")
 public class Video extends BaseEntity {
 
