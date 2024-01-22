80114a87dde930d69785fda0af72f16c59b5e02d
johyonghoon <johyonghoon01@gmail.com>
2024-01-22 14:29:55 +0900

    [#380] feat : User Update Pwd 컴포넌트 비밀번호 변경 규칙 적용

--- frontend/src/components/user/UserUpdatePwd.vue
+++ frontend/src/components/user/UserUpdatePwd.vue
@@ -4,10 +4,68 @@ import { useUserStore } from '@/stores/user.js'
 import { patchChangeUserPwd } from '@/api/user.js'
 
 const userStore = useUserStore()
+const formRef = ref(null)
 const original = ref(null)
 const password = ref(null)
 const confirm = ref(null)
 const dialog = ref(false)
+
+const originalRules = [
+  (value) => {
+    if (value) {
+      return true
+    } else {
+      return '현재 비밀번호를 입력해주세요.'
+    }
+  },
+]
+
+const passwordRules = [
+  (value) => {
+    if (value) {
+      return true
+    } else {
+      return '새 비밀번호를 입력해주세요.'
+    }
+  },
+  (value) => {
+    // 영문, 숫자, 특수문자 중 2가지 이상 포함
+    const complexityRegex = /^(?=.*[a-zA-Z])(?=.*\d|(?=.*\W)).{8,32}$/
+
+    if (complexityRegex.test(value)) {
+      return true
+    } else {
+      return '비밀번호는 영문/숫자/특수문자 중 2가지 이상을 포함하여 8자 이상 32자 이하로 입력해주세요.'
+    }
+  },
+]
+
+const passwordConfirmRules = [
+  (value) => {
+    if (value) {
+      return true
+    } else {
+      return '새 비밀번호 확인을 입력해주세요.'
+    }
+  },
+  (value) => {
+    // 영문, 숫자, 특수문자 중 2가지 이상 포함
+    const complexityRegex = /^(?=.*[a-zA-Z])(?=.*\d|(?=.*\W)).{8,32}$/
+
+    if (complexityRegex.test(value)) {
+      return true
+    } else {
+      return '비밀번호는 영문/숫자/특수문자 중 2가지 이상을 포함하여 8자 이상 32자 이하로 입력해주세요.'
+    }
+  },
+  (value) => {
+    if (value === password.value) {
+      return true
+    } else {
+      return '비밀번호가 일치하지 않습니다.'
+    }
+  }
+]
 
 const closeDialog = function () {
   dialog.value = false
@@ -15,27 +73,33 @@ const closeDialog = function () {
 
 const requestUserPwdChange = function () {
 
-  const payload = {
-    original: original.value,
-    password: password.value,
-    confirm: confirm.value
-  }
+  const isValid = formRef.value.validate()
 
-  const success = function (response) {
-    // 모달 - 비밀번호 변경을 완료했습니다.
-    dialog.value = true
+  isValid.then((response) => {
+    if (response.valid) {
+      const payload = {
+        original: original.value,
+        password: password.value,
+        confirm: confirm.value
+      }
 
-    // 입력되어 있는 데이터 지우기
-    original.value = null
-    password.value = null
-    confirm.value = null
-  }
+      const success = function (response) {
+        // 모달 - 비밀번호 변경을 완료했습니다.
+        dialog.value = true
 
-  const error = function (response) {
-    // 리턴되는 값에 따라 적용
-  }
+        // 입력되어 있는 데이터 지우기
+        original.value = null
+        password.value = null
+        confirm.value = null
+      }
 
-  patchChangeUserPwd(payload, success, error)
+      const error = function (response) {
+        // 리턴되는 값에 따라 적용
+      }
+
+      patchChangeUserPwd(payload, success, error)
+    }
+  })
 }
 </script>
 
@@ -44,9 +108,9 @@ const requestUserPwdChange = function () {
     <v-sheet width="90%" class="mx-auto my-10">
       <h3 class="mb-5">비밀번호 변경</h3>
       <v-form ref="formRef" fast-fail @submit.prevent="requestUserPwdChange">
-        <v-text-field v-model="original" label="현재 비밀번호"></v-text-field>
-        <v-text-field v-model="password" label="새 비밀번호"></v-text-field>
-        <v-text-field v-model="confirm" label="새 비밀번호 확인"></v-text-field>
+        <v-text-field v-model="original" type="password" label="현재 비밀번호"></v-text-field>
+        <v-text-field v-model="password" type="password" :rules="passwordRules" label="새 비밀번호"></v-text-field>
+        <v-text-field v-model="confirm" type="password" :rules="passwordConfirmRules" label="새 비밀번호 확인"></v-text-field>
         <div class="d-flex justify-center">
           <v-btn type="submit" class="d-flex justify-center mt-2 px-15" color="success">
             <h3>비밀번호 변경</h3>
