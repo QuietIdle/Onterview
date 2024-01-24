c0c33db2a538f581bd3c1b187ac0aec676cd99df
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2024-01-24 17:37:52 +0900

    remove: 코드 삭제

--- backend/src/main/java/com/quiet/onterview/OnterviewApplication.java
+++ backend/src/main/java/com/quiet/onterview/OnterviewApplication.java
@@ -2,17 +2,9 @@ package com.quiet.onterview;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class OnterviewApplication {
-
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(OnterviewApplication.class, args);
 	}
