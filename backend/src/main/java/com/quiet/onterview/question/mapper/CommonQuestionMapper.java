62107a1fe523f4995e03eec4da419d059bcbaf28
olrlozl <thakd221@naver.com>
2024-01-29 16:16:50 +0900

    [#S10P12A504-724-be-api] refactor: 필요없는 Mapper 주입 삭제

--- backend/src/main/java/com/quiet/onterview/question/mapper/CommonQuestionMapper.java
+++ backend/src/main/java/com/quiet/onterview/question/mapper/CommonQuestionMapper.java
@@ -1,23 +1,12 @@
 package com.quiet.onterview.question.mapper;
 
 import com.quiet.onterview.question.dto.request.CommonQuestionRequest;
-import com.quiet.onterview.question.dto.request.MyQuestionRequest;
 import com.quiet.onterview.question.dto.response.CommonQuestionResponse;
-import com.quiet.onterview.question.dto.response.MyAnswerAndVideoResponse;
-import com.quiet.onterview.question.dto.response.MyQuestionResponse;
 import com.quiet.onterview.question.entity.CommonQuestion;
-import com.quiet.onterview.question.entity.MyQuestion;
-import com.quiet.onterview.video.mapper.VideoMapper;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
-
 @Component
-@RequiredArgsConstructor
 public class CommonQuestionMapper {
-
-    private final VideoMapper videoMapper;
 
     public CommonQuestionResponse commonQuestionToCommonQuestionResponse(CommonQuestion commonQuestion) {
         return CommonQuestionResponse.builder()
