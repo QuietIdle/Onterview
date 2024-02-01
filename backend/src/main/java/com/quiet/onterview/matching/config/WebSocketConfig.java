d6d79092fa56083783b7f3c87638e7590accf23c
chlwodyd9829 <chlwodyd9829@naver.com>
2024-02-01 11:02:37 +0900

    [#S10P12A504-710] feat: 매칭 관련 WebSocket Config 및 Handler 정의

---
+++ backend/src/main/java/com/quiet/onterview/matching/config/WebSocketConfig.java
@@ -0,0 +1,29 @@
+package com.quiet.onterview.matching.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.HandshakeHandler;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final HandshakeHandler handshakeHandler;
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/api/meeting/matching")
+                .setAllowedOriginPatterns("*")
+                .setHandshakeHandler(handshakeHandler);
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/pub")
+                .enableSimpleBroker("/sub");
+    }
+}
