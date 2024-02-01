c43b77534e413db359ae33d0b72c8bda86a5ea3a
이경민 <74659491+meenyweeny@users.noreply.github.com>
2024-02-01 09:43:14 +0900

    feat: 커뮤니티 Entity 생성

---
+++ backend/src/main/java/com/quiet/onterview/community/entity/Comment.java
@@ -0,0 +1,53 @@
+package com.quiet.onterview.community.entity;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+import com.quiet.onterview.common.BaseEntity;
+import com.quiet.onterview.member.entity.Member;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Entity
+@Table(name = "COMMENT")
+public class Comment extends BaseEntity {
+
+    @Id
+    @Column(name = "COMMENT_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "ARTICLE_ID")
+    private Article article;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @Column(name="CONTENT", nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "PARENT_COMMENT_ID", referencedColumnName = "COMMENT_ID")
+    private Comment parent;
+
+    @OneToMany(fetch = LAZY, mappedBy = "parent", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private List<Comment> children = new ArrayList<>();
+}
---
+++ backend/src/main/java/com/quiet/onterview/community/entity/Like.java
@@ -0,0 +1,45 @@
+package com.quiet.onterview.community.entity;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+import com.quiet.onterview.member.entity.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Entity
+@Table(name = "LIKE")
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "LIKE_ID")
+    private Long likeId;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "ARTICLE_ID")
+    private Article article;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @Column(name="STATUS", nullable = false)
+    @ColumnDefault("like")
+    private String status;
+}
+
+
