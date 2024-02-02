09fb80b6443e323ae5372b48c335ba9fcc6f077c
이경민 <74659491+meenyweeny@users.noreply.github.com>
2024-02-02 15:25:31 +0900

    feat: 게시글 댓글만 조회 API 구현

---
+++ backend/src/main/java/com/quiet/onterview/community/dto/response/CommentResponse.java
@@ -0,0 +1,16 @@
+package com.quiet.onterview.community.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class CommentResponse {
+
+    Long parentCommentId;
+    Long commentId;
+    String writerNickname;
+    String content;
+    String writtenDate;
+    Boolean isMyComment;
+}
