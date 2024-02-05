fa9953650770f664d024e50296c5f98ce1908d42
eunjin <asd0755@naver.com>
2024-02-05 17:39:15 +0900

    [#885] fix : api token 수정

--- frontend/src/api/community.js
+++ frontend/src/api/community.js
@@ -3,41 +3,49 @@ import { useUserStore } from '@/stores/user'
 
 const api = localAxios()
 const userStore = useUserStore()
-api.defaults.headers.common["Authorization"] = userStore.accessToken
 
 const getAllPostList = function (order) {
+    api.defaults.headers.common["Authorization"] = userStore.accessToken
     return api.get(`/api/community?order=${order}`)
 }
 
 const getMyPostList = function (order) {
+    api.defaults.headers.common["Authorization"] = userStore.accessToken
     return api.get(`/api/community/my?order=${order}`)
 }
 
 const getPostDetail = function (articleId) {
+    api.defaults.headers.common["Authorization"] = userStore.accessToken
     return api.get(`/api/community/post/detail/${articleId}`)
 }
 
 const getCommentDetail = function (articleId) {
+    api.defaults.headers.common["Authorization"] = userStore.accessToken
     return api.get(`/api/community/comment/detail/${articleId}`)
 }
 
 const deleteDeleteMyPost = function (articleId) {
+    api.defaults.headers.common["Authorization"] = userStore.accessToken
     return api.delete(`/api/community/${articleId}`)
 }
 
 const patchUpdateMyPostContent = function (articleId, payload) {
+    api.defaults.headers.common["Authorization"] = userStore.accessToken
     return api.patch(`/api/community/${articleId}`, payload)
 }
 
 const postCreateMyComment = function (payload) {
+    api.defaults.headers.common["Authorization"] = userStore.accessToken
     return api.post(`/api/community/comment`, payload)
 }
 
 const deleteDeleteMyComment = function (commentId) {
+    api.defaults.headers.common["Authorization"] = userStore.accessToken
     return api.delete(`/api/community/comment/${commentId}`)
 }
 
 const patchLikePost = function (articleId) {
+    api.defaults.headers.common["Authorization"] = userStore.accessToken
     return api.patch(`/api/community/like/${articleId}`)
 }
 
--- frontend/src/api/question.js
+++ frontend/src/api/question.js
@@ -3,45 +3,54 @@ import { useUserStore } from "@/stores/user"
 
 const api = localAxios()
 const userStore = useUserStore()
-api.defaults.headers.common["Authorization"] = userStore.accessToken
 
 const getCommonQuestionList = function () {
+    api.defaults.headers.common["Authorization"] = userStore.accessToken
     return api.get(`/api/common-question-folder`)
 }
 
 const getMyQuestionList = function () {
+    api.defaults.headers.common["Authorization"] = userStore.accessToken
     return api.get(`/api/my-question-folder`) 
 }
 
 const postCreateMyQuestionFolder = function (payload) {
+    api.defaults.headers.common["Authorization"] = userStore.accessToken
     return api.post(`/api/my-question-folder`, payload)
 }
 
 const postCreateMyQuestion = function (payload) {
+    api.defaults.headers.common["Authorization"] = userStore.accessToken
     return api.post(`/api/my-question`, payload)
 }
 
 const deleteDeleteMyQuestionFolder = function (myQuestionFolderId) {
+    api.defaults.headers.common["Authorization"] = userStore.accessToken
     return api.delete(`/api/my-question-folder/${myQuestionFolderId}`)
 }
 
 const deleteDeleteMyQuestion = function (myQuestionId) {
+    api.defaults.headers.common["Authorization"] = userStore.accessToken
     return api.delete(`/api/my-question/${myQuestionId}`)
 }
 
 const patchUpdateMyQuestionFolder = function (myQuestionFolderId, payload) {
+    api.defaults.headers.common["Authorization"] = userStore.accessToken
     return api.patch(`/api/my-question-folder/${myQuestionFolderId}`, payload) // myQuestionFolder
 }
 
 const patchUpdateMyQuestion = function (myQuestionId, payload) {
+    api.defaults.headers.common["Authorization"] = userStore.accessToken
     return api.patch(`/api/my-question/${myQuestionId}`, payload) // myQuestion
 }
 
 const patchUpdateMyQuestionAnswer = function (myQuestionId, payload) {
+    api.defaults.headers.common["Authorization"] = userStore.accessToken
     return api.patch(`/api/my-question/answer/${myQuestionId}`, payload) // myQuestionAnswer
 }
 
 const patchMoveMyQuestion = function (payload) {
+    api.defaults.headers.common["Authorization"] = userStore.accessToken
     return api.patch(`/api/my-question/move`, payload)
 }
 
