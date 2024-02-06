09804e818111813eb2b90f539df1b06940397712
eunjin <asd0755@naver.com>
2024-02-06 12:41:31 +0900

    [#887] feat : CommunityWrite 컴포넌트 개발

--- frontend/src/components/community/CommunityWrite.vue
+++ frontend/src/components/community/CommunityWrite.vue
@@ -1,7 +1,65 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const tab = ref(1)
+const title = ref('영상을 선택해주세요')
+const content = ref('')
+</script>
 
 <template>
-  <div>글 작성</div>
+  <v-container>
+    <div class="text-grey mt-10">제목</div>
+    <div class="title mb-5" rows="5">{{ title }}</div>
+
+    <!-- Tab -->
+    <v-tabs v-model="tab" color="deep-purple-accent-4">
+      <v-tab :value="1">셀프 스피치</v-tab>
+      <v-tab :value="2">1인 모의면접</v-tab>
+      <v-tab :value="3">다인 모의면접</v-tab>
+    </v-tabs>
+
+    <v-window v-model="tab">
+      <v-window-item :value="1"> 셀프 스피치 </v-window-item>
+      <v-window-item :value="2"> 1인 모의면접 </v-window-item>
+      <v-window-item :value="3"> 다인 모의면접 </v-window-item>
+    </v-window>
+
+    <!-- 고민 내용 -->
+    <label for="content" class="text-grey mt-5">고민 내용</label>
+    <textarea
+      name="content"
+      rows="5"
+      :value="content"
+      placeholder="피드백 받고 싶은 내용을 작성해주세요"
+    ></textarea>
+
+    <!-- 버튼 -->
+    <div class="d-flex justify-center justify-space-evenly mt-5">
+      <v-btn color="grey" variant="tonal" @click="goCommunityList()"
+        >취소</v-btn
+      >
+      <v-btn color="#BB66FF" variant="tonal" @click="requestCreateMyPost()"
+        >글쓰기</v-btn
+      >
+    </div>
+  </v-container>
 </template>
 
-<style scoped></style>
+<style scoped>
+label {
+  display: block;
+}
+
+textarea,
+.title {
+  resize: none;
+  padding: 10px;
+  border: 5px solid #f0e8f6;
+  width: 100%;
+  outline-color: #f0e8f6;
+}
+
+textarea::placeholder {
+  color: #9e9e9e;
+}
+</style>
