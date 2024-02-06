410819463aa405b2a9431206c70ab04bfe90989f
chlwodyd9829 <chlwodyd9829@naver.com>
2024-02-06 17:17:40 +0900

    [#S10P12A504-903] fix: 영상 관련 요청 인증 처리 및 영상 정보 저장 에러 처리

--- backend/src/main/java/com/quiet/onterview/video/controller/VideoController.java
+++ backend/src/main/java/com/quiet/onterview/video/controller/VideoController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -61,8 +62,9 @@ public class VideoController {
     }
 
     @PostMapping("/delete")
-    public ResponseEntity<Void> deleteVideo(@RequestBody VideoDeleteRequest videos) {
-        videoService.deleteVideo(videos);
+    public ResponseEntity<Void> deleteVideo(@RequestBody VideoDeleteRequest videos,
+            @RequestHeader(value = "Authroziation") String token) {
+        videoService.deleteVideo(videos, token);
         return ResponseEntity.ok().build();
     }
 }
