62221885e410de47705e5940796277731258c009
zqwz <zqwz@naver.com>
2024-02-07 11:57:57 +0900

    [#S10P12A504-890] refactor: 컨트롤러, 서비스 로직 분리

--- onterview-storage/src/main/java/com/quiet/onterviewstorage/file/controller/FileController.java
+++ onterview-storage/src/main/java/com/quiet/onterviewstorage/file/controller/FileController.java
@@ -1,15 +1,13 @@
 package com.quiet.onterviewstorage.file.controller;
 
-import com.quiet.onterviewstorage.util.FileUtils;
 import com.quiet.onterviewstorage.file.dto.FileDto;
 import com.quiet.onterviewstorage.file.dto.FileDto.FileResponse;
-import java.io.File;
+import com.quiet.onterviewstorage.file.service.FileService;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.*;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,34 +18,24 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 public class FileController {
 
-    private final FileUtils fileUtils;
+    private final FileService fileService;
 
     @PostMapping("/file")
     public ResponseEntity<FileDto.FileResponse> imageUpload(
             @RequestPart("file") MultipartFile image
     ) throws IOException {
-        // TODO: 확장자명에 따라 영상/이미지로 나눠 저장
-        log.info(image.getOriginalFilename());
-        String uuidFileName = fileUtils.createUUIDFileName(
-                image.getOriginalFilename().split("\\.")[1]);
-        image.transferTo(Path.of(fileUtils.IMAGE_PATH + "/" + uuidFileName));
-        return ResponseEntity.ok(new FileResponse(fileUtils.IMAGE_PATH, uuidFileName));
+        FileResponse response = fileService.imageUpload(image);
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/file")
     public ResponseEntity<?> sendFile(
             @RequestParam("filePath") String filePath,
             @RequestParam("fileName") String fileName
-    ) throws IOException {
-        File file = new File(filePath + "/" + fileName);
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-        headers.add("Content-Disposition", "attachment" + file.getName());
-        String path = file.getPath();
-        byte[] body = Files.readAllBytes(Path.of(path));
+    ) {
+        Resource image = fileService.getImage(filePath, fileName);
         return ResponseEntity.ok()
-                .headers(headers)
-                .body(body);
+                .body(image);
     }
 
     @DeleteMapping("/file")
@@ -55,14 +43,7 @@ public class FileController {
             @RequestParam("filePath") String filePath,
             @RequestParam("fileName") String fileName
     ) {
-        log.info(filePath);
-        File file = new File(filePath + "/" + fileName);
-
-        if (file.exists()) {
-            file.delete();
-            return ResponseEntity.noContent().build();
-        }
-
-        return ResponseEntity.notFound().build();
+        fileService.deleteImage(filePath, fileName);
+        return ResponseEntity.noContent().build();
     }
 }
