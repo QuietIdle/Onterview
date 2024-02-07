8cec6fc8b4786f8f7dedc7896020db3028c66827
eunjin <asd0755@naver.com>
2024-02-07 14:40:20 +0900

    [#886] refactor : 커뮤니티 영상 테스트 데이터 삭제

--- frontend/src/components/community/CommunityStoageVideo.vue
+++ frontend/src/components/community/CommunityStoageVideo.vue
@@ -19,8 +19,7 @@ onMounted(() => {
 const requestSelfSpeechVideoList = async function () {
   try {
     const response = await getSelfSpeechVideoList()
-    console.log(response.data)
-    // videoList.value = response.data
+    videoList.value = response.data
   } catch (error) {
     console.error('셀프 스피치 영상목록 조회 실패', error)
   }
@@ -29,169 +28,13 @@ const requestSelfSpeechVideoList = async function () {
 const requestInterviewVideoList = async function () {
   try {
     const response = await getInterviewVideoList(props.roomType)
-    console.log(response.data)
     videoList.value = response.data
   } catch (error) {
     console.error('모의 면접 영상목록 조회 실패', error)
   }
 }
 
-const videoList = ref([
-  {
-    videoId: 1,
-    thumbnailUrl: {
-      fileId: 2,
-      originFilename: 'aaaa',
-      saveFilename:
-        'https://www.urbanbrush.net/web/wp-content/uploads/edd/2022/11/urbanbrush-20221108214712319041.jpg'
-    },
-    title: '제목입니다',
-    question: 'testCommonQuestion'
-  },
-  {
-    videoId: 2,
-    thumbnailUrl: {
-      fileId: 3,
-      originFilename: 'bbb',
-      saveFilename:
-        'https://png.pngtree.com/thumb_back/fh260/background/20230609/pngtree-three-puppies-with-their-mouths-open-are-posing-for-a-photo-image_2902292.jpg'
-    },
-    title: '제목입니다',
-    question: 'testCommonQuestion'
-  },
-  {
-    videoId: 3,
-    thumbnailUrl: {
-      fileId: 2,
-      originFilename: 'aaaa',
-      saveFilename:
-        'https://www.urbanbrush.net/web/wp-content/uploads/edd/2022/11/urbanbrush-20221108214712319041.jpg'
-    },
-    title: '제목입니다',
-    question: 'testCommonQuestion'
-  },
-  {
-    videoId: 4,
-    thumbnailUrl: {
-      fileId: 3,
-      originFilename: 'bbb',
-      saveFilename:
-        'https://png.pngtree.com/thumb_back/fh260/background/20230609/pngtree-three-puppies-with-their-mouths-open-are-posing-for-a-photo-image_2902292.jpg'
-    },
-    title: '제목이 몹시 길다아아아아테스트데이터가 테스트',
-    question: 'testCommonQuestion'
-  },
-  {
-    videoId: 88,
-    thumbnailUrl: {
-      fileId: 2,
-      originFilename: 'aaaa',
-      saveFilename:
-        'https://www.urbanbrush.net/web/wp-content/uploads/edd/2022/11/urbanbrush-20221108214712319041.jpg'
-    },
-    title: '제목이 몹시 길다아아아아테스트데이터가 테스트',
-    question: 'testCommonQuestion'
-  },
-  {
-    videoId: 89,
-    thumbnailUrl: {
-      fileId: 3,
-      originFilename: 'bbb',
-      saveFilename:
-        'https://png.pngtree.com/thumb_back/fh260/background/20230609/pngtree-three-puppies-with-their-mouths-open-are-posing-for-a-photo-image_2902292.jpg'
-    },
-    title: '제목이 몹시 길다아아아아테스트데이터가 테스트',
-    question: 'testCommonQuestion'
-  },
-  {
-    videoId: 90,
-    thumbnailUrl: {
-      fileId: 2,
-      originFilename: 'aaaa',
-      saveFilename:
-        'https://www.urbanbrush.net/web/wp-content/uploads/edd/2022/11/urbanbrush-20221108214712319041.jpg'
-    },
-    title: '제목이 몹시 길다아아아아테스트데이터가 테스트',
-    question: 'testCommonQuestion'
-  },
-  {
-    videoId: 91,
-    thumbnailUrl: {
-      fileId: 3,
-      originFilename: 'bbb',
-      saveFilename:
-        'https://png.pngtree.com/thumb_back/fh260/background/20230609/pngtree-three-puppies-with-their-mouths-open-are-posing-for-a-photo-image_2902292.jpg'
-    },
-    title: '제목이 몹시 길다아아아아테스트데이터가 테스트',
-    question: 'testCommonQuestion'
-  },
-  {
-    videoId: 92,
-    thumbnailUrl: {
-      fileId: 2,
-      originFilename: 'aaaa',
-      saveFilename:
-        'https://www.urbanbrush.net/web/wp-content/uploads/edd/2022/11/urbanbrush-20221108214712319041.jpg'
-    },
-    title: '제목이 몹시 길다아아아아테스트데이터가 테스트',
-    question: 'testCommonQuestion'
-  },
-  {
-    videoId: 93,
-    thumbnailUrl: {
-      fileId: 3,
-      originFilename: 'bbb',
-      saveFilename:
-        'https://png.pngtree.com/thumb_back/fh260/background/20230609/pngtree-three-puppies-with-their-mouths-open-are-posing-for-a-photo-image_2902292.jpg'
-    },
-    title: '제목이 몹시 길다아아아아테스트데이터가 테스트',
-    question: 'testCommonQuestion'
-  },
-  {
-    videoId: 94,
-    thumbnailUrl: {
-      fileId: 2,
-      originFilename: 'aaaa',
-      saveFilename:
-        'https://www.urbanbrush.net/web/wp-content/uploads/edd/2022/11/urbanbrush-20221108214712319041.jpg'
-    },
-    title: '제목이 몹시 길다아아아아테스트데이터가 테스트',
-    question: 'testCommonQuestion'
-  },
-  {
-    videoId: 95,
-    thumbnailUrl: {
-      fileId: 3,
-      originFilename: 'bbb',
-      saveFilename:
-        'https://png.pngtree.com/thumb_back/fh260/background/20230609/pngtree-three-puppies-with-their-mouths-open-are-posing-for-a-photo-image_2902292.jpg'
-    },
-    title: '제목입니다',
-    question: 'testCommonQuestion'
-  },
-  {
-    videoId: 96,
-    thumbnailUrl: {
-      fileId: 2,
-      originFilename: 'aaaa',
-      saveFilename:
-        'https://www.urbanbrush.net/web/wp-content/uploads/edd/2022/11/urbanbrush-20221108214712319041.jpg'
-    },
-    title: '제목입니다',
-    question: 'testCommonQuestion'
-  },
-  {
-    videoId: 97,
-    thumbnailUrl: {
-      fileId: 3,
-      originFilename: 'bbb',
-      saveFilename:
-        'https://png.pngtree.com/thumb_back/fh260/background/20230609/pngtree-three-puppies-with-their-mouths-open-are-posing-for-a-photo-image_2902292.jpg'
-    },
-    title: '제목입니다',
-    question: 'testCommonQuestion'
-  }
-])
+const videoList = ref([])
 
 const emit = defineEmits(['selectVideo'])
 
