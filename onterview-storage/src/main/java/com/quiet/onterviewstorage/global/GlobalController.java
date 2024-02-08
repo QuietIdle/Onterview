06febcbd6c20d869e2377eb6a0fdb0719b0ed216
zqwz <zqwz@naver.com>
2024-02-08 14:33:52 +0900

    [#S10P12A504-926] refactor: 파일 예외처리

--- onterview-storage/src/main/java/com/quiet/onterviewstorage/global/GlobalController.java
+++ onterview-storage/src/main/java/com/quiet/onterviewstorage/global/GlobalController.java
@@ -1,18 +1,33 @@
 package com.quiet.onterviewstorage.global;
 
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalController {
 
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<BaseExceptionResponse> handleBaseException(BaseException e) {
+        log.info("# error: " + e.getErrorCode().getMessage());
         return ResponseEntity.status(e.getErrorCode().getStatus())
                 .body(new BaseExceptionResponse(
                         e.getErrorCode().getStatus().value(),
                         e.getErrorCode().getMessage()
+                ));
+    }
+
+    @ExceptionHandler(IOException.class)
+    public ResponseEntity<BaseExceptionResponse> handleIOException(IOException e) {
+        log.info("# IO error: " + e.getMessage());
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                .body(new BaseExceptionResponse(
+                        HttpStatus.FORBIDDEN.value(),
+                        e.getMessage()
                 ));
     }
 }
