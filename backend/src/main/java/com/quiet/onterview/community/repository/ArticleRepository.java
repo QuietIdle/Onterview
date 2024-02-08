cab220c8ff7c59ca323b686adef38f0432e00477
이경민 <74659491+meenyweeny@users.noreply.github.com>
2024-02-08 14:15:41 +0900

    feat: 전체/내 게시글 조회 QueryDSL 적용

--- backend/src/main/java/com/quiet/onterview/community/repository/ArticleRepository.java
+++ backend/src/main/java/com/quiet/onterview/community/repository/ArticleRepository.java
@@ -1,48 +1,12 @@
 package com.quiet.onterview.community.repository;
 
 import com.quiet.onterview.community.entity.Article;
-import java.util.List;
+import com.quiet.onterview.community.repository.querydsl.ArticleQueryRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-
-    List<Article> findAllByOrderByCreateAtDesc();
-
-    List<Article> findAllByOrderByLikeCountDesc();
-
-    List<Article> findAllByOrderByCommentCountDesc();
-
-    List<Article> findAllByTitleContainingOrderByCreateAtDesc(String title);
-
-    List<Article> findAllByContentContainingOrderByCreateAtDesc(String content);
-
-    List<Article> findAllByTitleContainingOrderByLikeCountDesc(String title);
-
-    List<Article> findAllByContentContainingOrderByLikeCountDesc(String content);
-
-    List<Article> findAllByTitleContainingOrderByCommentCountDesc(String title);
-
-    List<Article> findAllByContentContainingOrderByCommentCountDesc(String content);
-
-    List<Article> findByMember_MemberIdOrderByCreateAtDesc(Long memberId);
-
-    List<Article> findByMember_MemberIdOrderByLikeCountDesc(Long memberId);
-
-    List<Article> findByMember_MemberIdOrderByCommentCountDesc(Long memberId);
-
-    List<Article> findByMember_MemberIdAndTitleContainingOrderByCreateAtDesc(Long memberId, String title);
-
-    List<Article> findByMember_MemberIdAndContentContainingOrderByCreateAtDesc(Long memberId, String content);
-
-    List<Article> findByMember_MemberIdAndTitleContainingOrderByLikeCountDesc(Long memberId, String title);
-
-    List<Article> findByMember_MemberIdAndContentContainingOrderByLikeCountDesc(Long memberId, String content);
-
-    List<Article> findByMember_MemberIdAndTitleContainingOrderByCommentCountDesc(Long memberId, String title);
-
-    List<Article> findByMember_MemberIdAndContentContainingOrderByCommentCountDesc(Long memberId, String content);
+public interface ArticleRepository extends JpaRepository<Article, Long>, ArticleQueryRepository {
 
     @Modifying
     @Query("update Article a set a.content = :content where a.articleId = :articleId")
