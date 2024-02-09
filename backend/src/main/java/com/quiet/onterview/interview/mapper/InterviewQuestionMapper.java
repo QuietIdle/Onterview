9723d550c07815b6c117a11e0559c343cfc469f4
zini9188 <zini9188@gmail.com>
2024-02-10 02:06:20 +0900

    refactor: 면접문항 응답 Video Optional 적용

--- backend/src/main/java/com/quiet/onterview/interview/mapper/InterviewQuestionMapper.java
+++ backend/src/main/java/com/quiet/onterview/interview/mapper/InterviewQuestionMapper.java
@@ -1,11 +1,11 @@
 package com.quiet.onterview.interview.mapper;
 
-import com.quiet.onterview.interview.dto.response.InterviewQuestionResponse;
 import com.quiet.onterview.interview.dto.response.InterviewQuestionCreateResponse;
+import com.quiet.onterview.interview.dto.response.InterviewQuestionResponse;
 import com.quiet.onterview.interview.entity.InterviewQuestion;
-import com.quiet.onterview.video.dto.response.VideoInformationResponse;
 import com.quiet.onterview.video.entity.Video;
 import com.quiet.onterview.video.mapper.VideoMapper;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -15,24 +15,28 @@ public class InterviewQuestionMapper {
 
     private final VideoMapper videoMapper;
 
-    public InterviewQuestionResponse interviewQuestionToInterviewQuestionResponse(InterviewQuestion interviewQuestion) {
+    public InterviewQuestionResponse interviewQuestionToInterviewQuestionResponse(
+            InterviewQuestion interviewQuestion) {
         Video video = interviewQuestion.getVideo();
 
         InterviewQuestionResponse.InterviewQuestionResponseBuilder responseBuilder =
                 InterviewQuestionResponse.builder()
                         .interviewQuestionId(interviewQuestion.getInterviewQuestionId())
-                        .commonQuestionId(interviewQuestion.getCommonQuestion().getCommonQuestionId())
+                        .commonQuestionId(
+                                interviewQuestion.getCommonQuestion().getCommonQuestionId())
                         .commonQuestion(interviewQuestion.getCommonQuestion().getCommonQuestion());
 
-        if (video != null) {
-            VideoInformationResponse videoInformationResponse = videoMapper.videoToInformationResponse(video);
-            responseBuilder.videoInformation(videoInformationResponse);
-        }
+        Optional.ofNullable(video)
+                .ifPresent(o -> responseBuilder.videoInformation(
+                        videoMapper.videoToInformationResponse(o))
+                );
 
         return responseBuilder.build();
     }
 
-    public InterviewQuestionCreateResponse interviewQuestionToInterviewQuestionCreateResponse(InterviewQuestion interviewQuestion) {
+    public InterviewQuestionCreateResponse interviewQuestionToInterviewQuestionCreateResponse(
+            InterviewQuestion interviewQuestion
+    ) {
         return InterviewQuestionCreateResponse.builder()
                 .interviewQuestionId(interviewQuestion.getInterviewQuestionId())
                 .commonQuestionId(interviewQuestion.getCommonQuestion().getCommonQuestionId())
