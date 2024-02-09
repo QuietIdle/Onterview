8dce296bdba7d168f45a7bc13851fb675aea50bb
zini9188 <zini9188@gmail.com>
2024-02-10 02:40:39 +0900

    refactor: 회원 셀프 스피치 response 스트림 적용

--- backend/src/main/java/com/quiet/onterview/question/service/MyQuestionFolderServiceImpl.java
+++ backend/src/main/java/com/quiet/onterview/question/service/MyQuestionFolderServiceImpl.java
@@ -6,21 +6,18 @@ import com.quiet.onterview.member.entity.Member;
 import com.quiet.onterview.member.repository.MemberRepository;
 import com.quiet.onterview.question.dto.request.MyQuestionFolderRequest;
 import com.quiet.onterview.question.dto.response.MyQuestionFolderResponse;
-import com.quiet.onterview.question.entity.MyQuestion;
 import com.quiet.onterview.question.entity.MyQuestionFolder;
 import com.quiet.onterview.question.exception.MyQuestionFolderNotFoundException;
 import com.quiet.onterview.question.mapper.MyQuestionFolderMapper;
 import com.quiet.onterview.question.mapper.MyQuestionMapper;
 import com.quiet.onterview.question.repository.MyQuestionFolderRepository;
 import com.quiet.onterview.video.dto.response.VideoStorageResponse;
-import com.quiet.onterview.video.entity.Video;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 @Service
 @Transactional
@@ -35,7 +32,8 @@ public class MyQuestionFolderServiceImpl implements MyQuestionFolderService {
     @Override
     @Transactional(readOnly = true)
     public List<MyQuestionFolderResponse> getMyQuestionFolder(Long memberId) {
-        List<MyQuestionFolder> myQuestionFolderList = myQuestionFolderRepository.findMyQuestionFolder(memberId);
+        List<MyQuestionFolder> myQuestionFolderList = myQuestionFolderRepository.findMyQuestionFolder(
+                memberId);
         return myQuestionFolderList.stream()
                 .map(myQuestionFolderMapper::myQuestionFolderToMyQuestionFolderResponse)
                 .toList();
@@ -43,32 +41,29 @@ public class MyQuestionFolderServiceImpl implements MyQuestionFolderService {
 
     @Override
     public List<VideoStorageResponse> getSelfVideoList(Long memberId) {
-        List<VideoStorageResponse> result = new ArrayList<>();
-        List<MyQuestionFolder> myQuestionFolderList = myQuestionFolderRepository.findMyQuestionFolder(memberId);
-        for (MyQuestionFolder myQuestionFolder : myQuestionFolderList) {
-            List<MyQuestion> myQuestionList = myQuestionFolder.getMyQuestionList();
-            for (MyQuestion myQuestion : myQuestionList) {
-                List<Video> videoList = myQuestion.getVideoList();
-                if (!videoList.isEmpty()) {
-                    for (Video video : videoList) {
-                        VideoStorageResponse videoStorageResponse = myQuestionMapper.videoToVideoStorageResponse(myQuestion, video);
-                        result.add(videoStorageResponse);
-                    }
-                }
-            }
-        }
-        return result;
+        return myQuestionFolderRepository.findMyQuestionFolder(memberId).stream()
+                .flatMap(myQuestionFolder -> myQuestionFolder.getMyQuestionList().stream())
+                .flatMap(myQuestion -> myQuestion.getVideoList().stream()
+                        .map(video ->
+                                myQuestionMapper.videoToVideoStorageResponse(myQuestion, video)
+                        )
+                )
+                .collect(Collectors.toList());
     }
 
     @Override
-    public void createMyQuestionFolder(Long memberId, MyQuestionFolderRequest myQuestionFolderRequest) {
-        Member member = memberRepository.findById(memberId).orElseThrow(() -> new BaseException(ErrorCode.MEMBERID_NOT_EXISTS));
-        MyQuestionFolder myQuestionFolder = myQuestionFolderMapper.myQuestionFolderRequestToEntity(member, myQuestionFolderRequest);
+    public void createMyQuestionFolder(Long memberId,
+            MyQuestionFolderRequest myQuestionFolderRequest) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new BaseException(ErrorCode.MEMBERID_NOT_EXISTS));
+        MyQuestionFolder myQuestionFolder = myQuestionFolderMapper.myQuestionFolderRequestToEntity(
+                member, myQuestionFolderRequest);
         myQuestionFolderRepository.save(myQuestionFolder);
     }
 
     @Override
-    public void updateMyQuestionFolder(Long myQuestionFolderId, MyQuestionFolderRequest myQuestionFolderRequest) {
+    public void updateMyQuestionFolder(Long myQuestionFolderId,
+            MyQuestionFolderRequest myQuestionFolderRequest) {
         MyQuestionFolder myQuestionFolder = myQuestionFolderRepository.findById(myQuestionFolderId)
                 .orElseThrow(MyQuestionFolderNotFoundException::new);
         Optional.ofNullable(myQuestionFolderRequest.getMyQuestionFolder())
