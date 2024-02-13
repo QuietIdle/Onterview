e4372493bba191dc0651c90fe0d456b6f74c7153
zqwz <zqwz@naver.com>
2024-02-13 14:20:10 +0900

    refactor: 안쓰는 코드 삭제

--- backend/src/main/java/com/quiet/onterview/video/repository/VideoRepository.java
+++ backend/src/main/java/com/quiet/onterview/video/repository/VideoRepository.java
@@ -1,11 +1,7 @@
 package com.quiet.onterview.video.repository;
 
-import com.quiet.onterview.interview.entity.RoomType;
-import com.quiet.onterview.question.entity.MyQuestion;
 import com.quiet.onterview.video.entity.Video;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface VideoRepository extends JpaRepository<Video, Long> {
 }
