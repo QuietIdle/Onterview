a0980e8873655ee848028aca669098d61eaa2997
zqwz <zqwz@naver.com>
2024-02-13 14:19:14 +0900

    refactor: 안쓰는 코드 삭제

--- backend/src/main/java/com/quiet/onterview/video/repository/VideoRepository.java
+++ backend/src/main/java/com/quiet/onterview/video/repository/VideoRepository.java
@@ -8,24 +8,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface VideoRepository extends JpaRepository<Video, Long> {
-
-    List<Video> findAllByMyQuestion(MyQuestion myQuestion);
-
-    @Query("SELECT v FROM Video v "
-            + "JOIN v.myQuestion q on v.myQuestion.myQuestionId = q.myQuestionId "
-            + "JOIN q.myQuestionFolder f on f.myQuestionFolderId = q.myQuestionFolder.myQuestionFolderId "
-            + "and f.member.memberId =:memberId")
-    List<Video> findAllSelfVideoByMember(Long memberId);
-
-    @Query("SELECT v FROM Video v "
-            + "join InterviewQuestion iq on v.interviewQuestion.interviewQuestionId = iq.interviewQuestionId "
-            + "join Interviewee iv on iq.interviewee.intervieweeId = iv.intervieweeId and iv.member.memberId =:memberId "
-            + "join InterviewRoom ivr on iv.interviewRoom.interviewRoomId = ivr.interviewRoomId and ivr.roomType=:type")
-    List<Video> findAllMultiVideoByMember(Long memberId, RoomType type);
-
-    @Query("SELECT v FROM Video v "
-            + "join InterviewQuestion iq on v.interviewQuestion.interviewQuestionId = iq.interviewQuestionId "
-            + "join Interviewee iv on iq.interviewee.intervieweeId = iv.intervieweeId and iv.member.memberId =:memberId "
-            + "join InterviewRoom ivr on iv.interviewRoom.interviewRoomId = ivr.interviewRoomId and ivr.roomType=:type")
-    List<Video> findAllSingleVideoByMember(Long memberId, RoomType type);
 }
