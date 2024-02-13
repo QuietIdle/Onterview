eb1ee213e6478cd06f42e2b066ab36d846ef3a10
chlwodyd9829 <chlwodyd9829@naver.com>
2024-02-13 13:54:25 +0900

    [#S10P12A504-1036] fix: BaseException 외에 Exception Message return 하도록 변경

--- backend/src/main/java/com/quiet/onterview/common/BaseExceptionHandler.java
+++ backend/src/main/java/com/quiet/onterview/common/BaseExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.quiet.onterview.common;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,12 +10,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RequiredArgsConstructor
 public class BaseExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
+    @ExceptionHandler(BaseException.class)
     private ResponseEntity<BaseExceptionResponse> GlobalException(BaseException baseException) {
         return ResponseEntity.status(baseException.getErrorCode().getStatusCode())
                 .body(BaseExceptionResponse.builder()
                         .errorCode(baseException.getErrorCode().getStatusCode().value())
                         .errorMessage(baseException.getErrorCode().getMessage())
                         .build());
+    }
+
+    @ExceptionHandler(Exception.class)
+    private ResponseEntity<String> exceptionHandle(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
     }
 }
