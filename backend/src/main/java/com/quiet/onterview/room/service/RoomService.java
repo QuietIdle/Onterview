19bccab467422beef6a30457e771ea1aef884064
j-yong98 <chlwodyd9829@gmail.com>
2024-02-14 18:14:40 +0900

    [#S10P12A504-9999] add: log 기능 추가

--- backend/src/main/java/com/quiet/onterview/room/service/RoomService.java
+++ backend/src/main/java/com/quiet/onterview/room/service/RoomService.java
@@ -51,7 +51,7 @@ public class RoomService {
                 RoomLeaveResponse.builder().idx(idx).build());
     }
 
-    public void process(String sessionId, UserRequestMessage userRequestMessage, String user) {
+    public void process(String sessionId, UserRequestMessage userRequestMessage) {
         RoomStatus roomStatus = userRequestMessage.getType();
         Integer idx = userRequestMessage.getIndex();
         RoomProgressResponseBuilder progressResponseBuilder = RoomProgressResponse.builder()
@@ -75,12 +75,11 @@ public class RoomService {
         if (type == ENTER || type == FINISH) {
             progressResponseBuilder.orders(roomRepository.shuffle(sessionId));
             progressResponseBuilder.question(roomRepository.getQuestion(sessionId));
+        } else if (type == SAVED) {
+            progressResponseBuilder.number(null);
+            progressResponseBuilder.index(idx);
         }
-        if (type == SAVED) {
-            messageService.announceToUser(ROOM_PREFIX + sessionId, user, progressResponseBuilder.build());
-        } else {
-            messageService.announceAll(ROOM_PREFIX + sessionId, progressResponseBuilder.build());
-        }
+        messageService.announceAll(ROOM_PREFIX + sessionId, progressResponseBuilder.build());
     }
 
     private RoomStatus save(String sessionId, UserRequestMessage userRequestMessage) {
@@ -94,7 +93,7 @@ public class RoomService {
                 VideoInformationRequest.builder()
                         .interviewQuestionId(
                                 interviewQuestionCreateResponses.get(
-                                        questionIndex.getAndIncrement())
+                                                questionIndex.getAndIncrement())
                                         .getInterviewQuestionId()
                         )
                         .title(video.getTitle())
