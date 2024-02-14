f01ec24ad3392c17c12750572e2ffb09360f856c
김승배 <ksshc@snu.ac.kr>
2024-02-14 15:03:07 +0900

    [#1006] fix : 다인 면접 인원 수정

--- frontend/src/components/interview/interviewMultiWait.vue
+++ frontend/src/components/interview/interviewMultiWait.vue
@@ -70,7 +70,7 @@ const startMatch = function () {
         headers,
         JSON.stringify({
           roomId: interviewStore.stompType,
-          matchCount: 1,
+          matchCount: 4,
         })
       )
     },
